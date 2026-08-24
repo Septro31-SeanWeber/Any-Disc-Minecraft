6b8d9bbac6abd72b9fae41faab5a078afd307917
Septro31-SeanWeber <sdweb375@gmail.com>
2026-08-23 19:52:47 -0700

    force build.rs error to view output, very verbose, and cargo cleaning

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=assets/icon.ico");
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+    let target = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    println!("cargo:warning= target is {}", target);
+    if target == "windows" {
         let mut res = winresource::WindowsResource::new();
         res.set_icon("assets/icon.ico");
         res.compile().unwrap();
@@ -9,4 +11,5 @@ fn main() {
     } else {
         println!("cargo:warning= windows not target os...");
     }
+    panic!("--- FORCED CRASH TO READ LOGS ---");
 }
