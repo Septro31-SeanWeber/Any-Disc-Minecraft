5a7c56e4b84e70890dd51c5ae901a11968f2c20d
Septro31-SeanWeber <sdweb375@gmail.com>
2026-08-23 19:20:34 -0700

    cargo instructions

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=assets/icon.ico");
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         let mut res = winresource::WindowsResource::new();
         res.set_icon("assets/icon.ico");
